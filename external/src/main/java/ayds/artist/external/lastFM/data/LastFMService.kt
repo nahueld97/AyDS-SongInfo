cb3d21951911ef2f5db4ab7faf12fff707926ceb
nahueld97 <nahueld97@outlook.com>
2024-05-29 17:45:43 -0300

    ups2

--- external/src/main/java/ayds/artist/external/lastFM/data/LastFMService.kt
+++ external/src/main/java/ayds/artist/external/lastFM/data/LastFMService.kt
@@ -1,7 +1,5 @@
 package ayds.artist.external.lastFM.data
 
-import ayds.songinfo.moredetails.domain.entity.ArtistBiography
-
 interface LastFMService {
-    fun getArticleByArtistName(artistName: String):ArtistBiography
+    fun getArticleByArtistName(artistName: String):LastFmBiography
 }
