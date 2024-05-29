e5ac3abfcbaff05d805c4a1c0a3416e113fa5df4
Luz255181 <mira_luz_explo@hotmail.com>
2024-05-29 15:43:34 -0300

    Test moredetails repository y presenter

---
+++ app/src/test/java/ayds/songinfo/moredetails/data/ArtistInfoRepositoryTest.kt
@@ -0,0 +1,57 @@
+package ayds.songinfo.moredetails.data
+
+import ayds.songinfo.moredetails.data.external.ExternalService
+import ayds.songinfo.moredetails.data.local.LocalService
+import ayds.songinfo.moredetails.domain.entity.ArtistBiography
+import ayds.songinfo.moredetails.domain.repository.ArtistInfoRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert
+import org.junit.Test
+
+class ArtistInfoRepositoryTest {
+
+    private var local: LocalService = mockk(relaxUnitFun = true)
+    private var external: ExternalService = mockk(relaxUnitFun = true)
+
+    private val artistInfoRepository: ArtistInfoRepository =
+        ArtistInfoRepositoryImpl(local, external)
+
+    @Test
+    fun getArtistInfoLocalTest() {
+        val artistBiography = ArtistBiography("artistName", "biography", "articleUrl")
+
+        every { local.getArticle("artistName") } returns artistBiography
+
+        val result = artistInfoRepository.getArtistInfo("artistName")
+
+        Assert.assertEquals(artistBiography, result)
+        Assert.assertEquals(artistBiography.isLocallyStored, true)
+    }
+
+    @Test
+    fun getArtistInfoExternalNullBiographyTest() {
+        val artistBiography = ArtistBiography("artistName", "", "articleUrl")
+
+        every { local.getArticle("artistName") } returns null
+        every { external.getArticleByArtistName("artistName") } returns artistBiography
+
+        val result = artistInfoRepository.getArtistInfo("artistName")
+
+        Assert.assertEquals(artistBiography, result)
+        Assert.assertEquals(artistBiography.isLocallyStored, false)
+    }
+
+@Test
+fun getArtistInfoExternalTest(){
+    val artistBiography = ArtistBiography("artistName", "biography", "articleUrl")
+
+    every { local.getArticle("artistName") } returns null
+    every { external.getArticleByArtistName("artistName") } returns artistBiography
+
+    val result = artistInfoRepository.getArtistInfo("artistName")
+
+    Assert.assertEquals(artistBiography, result)
+    Assert.assertEquals(artistBiography.isLocallyStored, false)
+}
+}
--- app/src/test/java/ayds/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
+++ app/src/test/java/ayds/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
@@ -1,5 +1,36 @@
 package ayds.songinfo.moredetails.presentation
 
+import ayds.songinfo.moredetails.domain.entity.ArtistBiography
+import ayds.songinfo.moredetails.domain.repository.ArtistInfoRepository
+import ayds.songinfo.moredetails.presentation.presenter.*
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
 class OtherInfoPresenterTest {
+    private val artistInfoRepository: ArtistInfoRepository = mockk()
+    private val artistBiographyHelper: ArtistBiographyHelper = mockk()
+    private val otherInfoPresenter: OtherInfoPresenter =
+        OtherInfoPresenterImpl(artistInfoRepository, artistBiographyHelper)
+
+    @Test
+    fun getArtistInfoTest() {
+        val artistBiography = ArtistBiography("artistName", "biography", "articleUrl")
+        every { artistInfoRepository.getArtistInfo("artistName") } returns artistBiography
+        every { artistBiographyHelper.getArtistDescriptionText(artistBiography) } returns "description"
+        val onUiStateHandler: (ArtistBiographyUiState) -> Unit = mockk(relaxed = true)
+        otherInfoPresenter.artistBiographyObservable.subscribe(onUiStateHandler)
+
+        otherInfoPresenter.getArtistInfo("artistName")
+
+        verify { onUiStateHandler(
+            ArtistBiographyUiState(
+                "artistName",
+                "description",
+                "articleUrl"
+            )
+        ) }
+    }
 
 }
